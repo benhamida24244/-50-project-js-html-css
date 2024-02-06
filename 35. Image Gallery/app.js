8ec87f3d8bee6b59baad740503ee8b1fbe6649d6
Benhamida Mohammed <mbenhamida136@gmail.com>
2024-02-06 20:03:39 +0100

    Update app.js

--- 35. Image Gallery/app.js
+++ 35. Image Gallery/app.js
@@ -2,6 +2,7 @@ let images = document.querySelectorAll("img");
 let wrapper = document.getElementById("wrapper");
 let imgWrapper = document.getElementById("fullImg");
 let close = document.querySelector("span");
+
 images.forEach((img, index) => {
   img.addEventListener("click", () => {
     openModal(`images/img${index}.jpg`);
